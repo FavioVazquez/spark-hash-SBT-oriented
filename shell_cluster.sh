849f0f02d6653123f77bee3238309fc4bf907a2a
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-20 13:41:15 -0430

    - Deleted unnecessary files

--- shell_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-$SPARK_HOME/bin/spark-shell \
-  --master yarn-client \
-  --num-executors 8 \
-  --driver-memory 4g \
-  --executor-memory 400g \
-  --executor-cores 60 \
-  --jars target/spark-hash-0.1.2.jar $@
--- shell_local.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-$SPARK_HOME/bin/spark-shell \
-  --master local[*] \
-  --executor-memory 8G \
-  --driver-memory 8G \
-  --jars target/scala-2.10/spark-hash.jar $@
